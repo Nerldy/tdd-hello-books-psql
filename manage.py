86d8d7dc6b7512a245e67dcd0290791673584119
nerldy <arnodhis@gmail.com>
2018-06-27 20:58:14 +0300

    add admin script

--- manage.py
+++ manage.py
@@ -2,6 +2,34 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db
 from app.models import User
+import getpass
+from app.auth.helper_funcs import format_inputs
+import re
+
+admin_user_schema = {
+	'username': {
+		'type': 'string',
+		'required': True,
+		'minlength': 2
+	},
+	'email': {
+		'type': 'string',
+		'required': True,
+		'maxlength': 100
+	},
+	'password': {
+		'type': 'string',
+		'required': True
+	},
+	'confirm_password': {
+		'type': 'string',
+		'required': True,
+	},
+	'is_admin': {
+		'type': 'boolean',
+		'required': True
+	}
+}
 
 # Initializing the manager
 manager = Manager(app)
@@ -12,24 +40,43 @@ migrate = Migrate(app, db)
 # Add the flask migrate
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
-def create_superuser():
+def create_admin():
 	"""creates admin"""
-	username = input('Enter your username: ')
+	username = format_inputs(input('Enter your username: '))
 	email = input('Enter your email: ')
-	password = input('Enter your password: ')
-	confirm_password = input('Enter your password again: ')
+	password = getpass.getpass('Enter password')
+	confirm_password = getpass.getpass('Enter your password again: ')
 
-	admin_user = {
-		'username': username,
+	# check length of username
+	if len(username) < 2:
+		return print('username cannot be empty and less than two characters')
 
-	}
+	# confirm passwords match
+	if password != confirm_password:
+		return print('passwords do not match')
+
+	# confirm length of password is not less than four
+	if len(password) < 4:
+		return print('password length must be four or more')
+
+	# validate if email matches the standard
+	validate_email = re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)
+	if validate_email is None:
+		return print("email format must have a local part, the “@” symbol, and the domain")
 
 	try:
 		admin = User(
 			username=username,
-			email = email
+			email=email,
+			password=password,
+			is_admin=True
 		)
+		admin.save()
+		return print(f'Admin {username} successfully created')
+	except Exception as e:
+		print(e)
 
 
 # Run the manager
